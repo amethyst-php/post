fb1b9a733bb3b753e5ed529ee104f3a5a7861980
railken <railken@protonmail.ch>
2019-07-14 13:05:00 +0200

    phpdoc

--- src/Managers/PostManager.php
+++ src/Managers/PostManager.php
@@ -5,6 +5,14 @@ namespace Amethyst\Managers;
 use Amethyst\Common\ConfigurableManager;
 use Railken\Lem\Manager;
 
+/**
+ * @method \Amethyst\Models\Post newEntity()
+ * @method \Amethyst\Schemas\PostSchema getSchema()
+ * @method \Amethyst\Repositories\PostRepository getRepository()
+ * @method \Amethyst\Serializers\PostSerializer getSerializer()
+ * @method \Amethyst\Validators\PostValidator getValidator()
+ * @method \Amethyst\Authorizers\PostAuthorizer getAuthorizer()
+ */
 class PostManager extends Manager
 {
     use ConfigurableManager;
